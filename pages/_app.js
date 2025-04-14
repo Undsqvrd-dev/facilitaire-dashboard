1c0edd84d15802c0e89ee9e767177d5554700082
Steijn de Ridder <steijn@undsqvrd.nl>
2025-04-14 20:04:49 +0200

    Fix Tailwind configuratie en styling issues

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
-import '@/styles/globals.css';
+import '../styles/globals.css';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    // Force a repaint to ensure styles are applied
+    document.body.style.display = 'none';
+    document.body.offsetHeight;
+    document.body.style.display = '';
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
