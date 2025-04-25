d4bd1f5ddabe226ac917cf1362b2dddb80209ed2
Steijn de Ridder <steijn@undsqvrd.nl>
2025-04-25 10:09:56 +0200

    Fix TopBar component en update credit tekst

--- components/TopBar.js
+++ components/TopBar.js
@@ -1,5 +1,14 @@
-  <div className="credit">
-    <a href="https://www.undsqvrd.com" target="_blank" rel="noopener noreferrer">
-      Powered by UNDSQVRD
-    </a>
-  </div> 
+const TopBar = () => {
+  return (
+    <div className="top-bar">
+      <h1>Facilitaire Bedrijven Dashboard</h1>
+      <div className="credit">
+        <a href="https://www.undsqvrd.com" target="_blank" rel="noopener noreferrer">
+          Powered by UNDSQVRD
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default TopBar; 
